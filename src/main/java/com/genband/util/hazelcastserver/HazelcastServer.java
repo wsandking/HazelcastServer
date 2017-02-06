ec6cc82090c84ea6030d52d52ee905c9572dfd72
Sen Wang <Sen.Wang@kandy.io>
2017-02-06 13:24:35 -0500

    Initial build

---
+++ src/main/java/com/genband/util/hazelcastserver/HazelcastServer.java
@@ -0,0 +1,51 @@
+package com.genband.util.hazelcastserver;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+import com.genband.util.hazelcastserver.config.HazelcastServerConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+/**
+ * Entry point of the hazelcast server application
+ * 
+ * @author sewang
+ *
+ */
+public class HazelcastServer {
+
+  private static Logger log = Logger.getLogger(HazelcastServer.class.getName());
+
+
+  public static void main(String args[]) {
+
+    BasicConfigurator.configure();
+
+    HazelcastInstance instance =
+        Hazelcast.newHazelcastInstance(new HazelcastServerConfig().composeConfiguration());
+
+    /*
+     * Testing code, will be removed when things got stable
+     * 
+     * Map<String, String> theMap = instance.getMap("TempMap");
+     * theMap.put("haha", "notveryhaha");
+     * log.info(String.format("%s : %s", "haha", theMap.get("haha")));
+     * 
+     * try {
+     * while (null != theMap.get("haha")) {
+     * Thread.sleep(2000);
+     * log.info(String.format("loop %s : %s", "haha", theMap.get("haha")));
+     * }
+     * } catch (InterruptedException e) {
+     * // TODO Auto-generated catch block
+     * e.printStackTrace();
+     * }
+     */
+
+    log.info(String.format("Hazelcast server: %s started successfully", instance.getName()));
+
+
+  }
+
+}
---
+++ src/main/java/com/genband/util/hazelcastserver/config/HazelcastServerConfig.java
@@ -0,0 +1,77 @@
+package com.genband.util.hazelcastserver.config;
+
+import java.io.InputStream;
+
+import org.apache.log4j.Logger;
+ 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.DiscoveryStrategyConfig;
+import com.hazelcast.config.XmlConfigBuilder;
+
+/**
+ * Initialize the configuration for hazelcast
+ * 
+ * @author sewang
+ *
+ */
+public class HazelcastServerConfig {
+
+  private static Logger log = Logger.getLogger(HazelcastServerConfig.class.getName());
+
+  /**
+   * Load it from environment variable
+   * 
+   * @param cfg
+   */
+  private void loadProperties(Config cfg) {
+
+    for (DiscoveryStrategyConfig dsc : cfg.getNetworkConfig().getJoin().getDiscoveryConfig()
+        .getDiscoveryStrategyConfigs()) {
+
+      /**
+       * Part 1: cluster discovery through service
+       */
+      if ("com.noctarius.hazelcast.kubernetes.HazelcastKubernetesDiscoveryStrategy"
+          .equals(dsc.getClassName())) {
+
+        log.info("Initialize kubernetes hazelcast server. ");
+
+        /**
+         * Load system level properties
+         */
+        if (null != System.getenv("NAMESPACE")) {
+          dsc.addProperty("namespace", System.getenv("NAMESPACE"));
+        }
+        
+        if (null != System.getenv("SERVICE_NAME")) {
+          dsc.addProperty("service-name", System.getenv("SERVICE_NAME"));
+        }
+        
+        if (null != System.getenv("SERVICE_LABEL_NAME")) {
+          dsc.addProperty("service-label-name", System.getenv("SERVICE_LABEL_NAME"));
+        }
+
+        if (null != System.getenv("SERVICE_LABEL_VALUE")) {
+          dsc.addProperty("service-label-value", System.getenv("SERVICE_LABEL_VALUE"));
+        }
+        
+      }
+
+    }
+
+  }
+
+  public Config composeConfiguration() {
+
+    Config cfg = null;
+
+    InputStream in = this.getClass().getClassLoader().getResourceAsStream("hazelcast.xml");
+    cfg = new XmlConfigBuilder(in).build();
+
+    loadProperties(cfg);
+
+    return cfg;
+
+  }
+
+}
