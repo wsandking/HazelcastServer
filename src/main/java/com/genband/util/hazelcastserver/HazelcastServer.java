4ec235fd524f219ae4f145884b03b1a77fce9e98
Sen Wang <Sen.Wang@kandy.io>
2017-03-27 15:36:33 -0400

    hazelcast involving logging utility.

--- src/main/java/com/genband/util/hazelcastserver/HazelcastServer.java
+++ src/main/java/com/genband/util/hazelcastserver/HazelcastServer.java
@@ -1,8 +1,8 @@
 package com.genband.util.hazelcastserver;
 
-import org.apache.log4j.Logger;
-
 import com.genband.util.hazelcastserver.config.HazelcastServerConfig;
+import com.genband.util.log.slf4j.GbLogger;
+import com.genband.util.log.slf4j.GbLoggerFactory;
 // import com.genband.util.hazelcastserver.test.KubernetesHazelcastTest;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
@@ -15,22 +15,11 @@ import com.hazelcast.core.HazelcastInstance;
  */
 public class HazelcastServer {
 
-    private static Logger log = Logger.getLogger(HazelcastServer.class.getName());
+    private static GbLogger log = GbLoggerFactory.getGbLogger(HazelcastServer.class.getName());
 
     public static void main(String args[]) {
 
         HazelcastInstance instance = Hazelcast.newHazelcastInstance(new HazelcastServerConfig().composeConfiguration());
-
-        // HazelcastInstance instance = Hazelcast.newHazelcastInstance();
-        
-        /*
-         * Testing code, will be removed when things got stable Map<String, String> theMap = instance.getMap("TempMap");
-         * theMap.put("haha", "notveryhaha"); log.info(String.format("%s : %s", "haha", theMap.get("haha"))); try {
-         * while (null != theMap.get("haha")) { Thread.sleep(2000); log.info(String.format("loop %s : %s", "haha",
-         * theMap.get("haha"))); } } catch (InterruptedException e) { // TODO Auto-generated catch block
-         * e.printStackTrace(); }
-         */
-
         log.info(String.format("Hazelcast server: %s started successfully", instance.getName()));
 
     }
--- src/main/java/com/genband/util/hazelcastserver/config/HazelcastServerConfig.java
+++ src/main/java/com/genband/util/hazelcastserver/config/HazelcastServerConfig.java
@@ -2,8 +2,8 @@ package com.genband.util.hazelcastserver.config;
 
 import java.io.InputStream;
 
-import org.apache.log4j.Logger;
-
+import com.genband.util.log.slf4j.GbLogger;
+import com.genband.util.log.slf4j.GbLoggerFactory;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.DiscoveryStrategyConfig;
 import com.hazelcast.config.MapConfig;
@@ -17,80 +17,69 @@ import com.hazelcast.config.XmlConfigBuilder;
  */
 public class HazelcastServerConfig {
 
-	private static Logger log = Logger.getLogger(HazelcastServerConfig.class.getName());
+    private static GbLogger log = GbLoggerFactory.getGbLogger(HazelcastServerConfig.class.getName());
 
-	/**
-	 * Load it from environment variable
-	 * 
-	 * @param cfg
-	 */
-	private void loadProperties(Config cfg) {
+    /**
+     * Load it from environment variable
+     * 
+     * @param cfg
+     */
+    private void loadProperties(Config cfg) {
+        /**
+         * 
+         * Hazelcast server map configuration
+         * 
+         */
+        String timeToLive = System.getenv("MAP_TIME_TO_LIVE");
+        String nameSpace = System.getenv("NAMESPACE");
+        String serviceName = System.getenv("SERVICE_NAME");
 
-		/**
-		 * 
-		 * Hazelcast server map configuration
-		 * 
-		 */
-		String timeToLive = System.getenv("MAP_TIME_TO_LIVE");
-		String nameSpace = System.getenv("NAMESPACE");
-		String serviceName = System.getenv("SERVICE_NAME");
+        if (null != cfg.getMapConfig("default")) {
 
-		if (null != cfg.getMapConfig("MobilePushEventMap")) {
+            log.info("Configuring Mobile Push Event Map ");
+            MapConfig mapCfg = cfg.getMapConfig("default");
 
-			log.info("Configuring Mobile Push Event Map ");
-			MapConfig mapCfg = cfg.getMapConfig("MobilePushEventMap");
+            if (null != timeToLive && timeToLive.matches("^-?\\d+$")) {
+                mapCfg.setTimeToLiveSeconds(Integer.parseInt(System.getenv("MAP_TIME_TO_LIVE")));
+            }
 
-			if (null != timeToLive && timeToLive.matches("^-?\\d+$")) {
+        }
+        /**
+         * 
+         * Hazelcast Discovery
+         * 
+         */
+        for (DiscoveryStrategyConfig dsc : cfg.getNetworkConfig().getJoin().getDiscoveryConfig()
+                .getDiscoveryStrategyConfigs()) {
 
-				mapCfg.setTimeToLiveSeconds(Integer.parseInt(System.getenv("MAP_TIME_TO_LIVE")));
+            /**
+             * Part 1: cluster discovery through service
+             */
+            if ("com.noctarius.hazelcast.kubernetes.HazelcastKubernetesDiscoveryStrategy".equals(dsc.getClassName())) {
+                log.info("Initialize kubernetes hazelcast server. ");
+                /**
+                 * Load system level properties
+                 */
+                if (null != nameSpace) {
+                    dsc.addProperty("namespace", nameSpace);
+                    log.info("Properties namespace: " + nameSpace);
+                }
+                if (null != serviceName) {
+                    dsc.addProperty("service-name", serviceName);
+                    log.info("Properties service-name: " + serviceName);
+                }
+            }
+        }
+    }
 
-			}
-
-		}
-		/**
-		 * 
-		 * Hazelcast Discovery
-		 * 
-		 */
-		for (DiscoveryStrategyConfig dsc : cfg.getNetworkConfig().getJoin().getDiscoveryConfig()
-				.getDiscoveryStrategyConfigs()) {
-
-			/**
-			 * Part 1: cluster discovery through service
-			 */
-			if ("com.noctarius.hazelcast.kubernetes.HazelcastKubernetesDiscoveryStrategy".equals(dsc.getClassName())) {
-
-				log.info("Initialize kubernetes hazelcast server. ");
-				/**
-				 * Load system level properties
-				 */
-				if (null != nameSpace) {
-
-					dsc.addProperty("namespace", nameSpace);
-					log.info("Properties namespace: " + nameSpace);
-
-				}
-				if (null != serviceName) {
-
-					dsc.addProperty("service-name", serviceName);
-					log.info("Properties service-name: " + serviceName);
-
-				}
-			}
-		}
-	}
-
-	public Config composeConfiguration() {
-
-		Config cfg = null;
-		cfg = new Config();
-		InputStream in = this.getClass().getClassLoader().getResourceAsStream("hazelcast.xml");
-		cfg = new XmlConfigBuilder(in).build();
-		loadProperties(cfg);
-		log.info(cfg.toString());
-
-		return cfg;
-
-	}
+    public Config composeConfiguration() {
+        Config cfg = null;
+        cfg = new Config();
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream("hazelcast.xml");
+        cfg = new XmlConfigBuilder(in).build();
+        loadProperties(cfg);
+        log.info(cfg.toString());
+        return cfg;
+    }
 
 }
